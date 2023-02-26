82492313808f91a46472b32db6032da6d45b5a7f
ryowuandjanet <ryowu0329@gmail.com>
2023-02-26 17:32:13 +0000

    75 尚硅谷 Vue3 todoList案例拆分组件

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <Header />
+      <List />
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import Header from './components/Header.vue'
+  import List from './components/List.vue'
+  import Footer from './components/Footer.vue'
+  export default defineComponent ({
+    name: 'App',
+    components: {
+      Header,
+      List,
+      Footer
+    }
+
+  })
+</script>
+
+<style>
+  /*app*/
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+</style>
